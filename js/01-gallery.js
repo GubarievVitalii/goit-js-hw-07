0768e245bb2c5ae0a15184816e839849e2c5991e
gubariev-vitalii <100084830+GubarievVitalii@users.noreply.github.com>
2022-08-12 21:27:50 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
+let instance;
 // Change code below this line
 
-console.log(galleryItems);
+const listGallery = document.querySelector(".gallery");
+
+const newArray = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__item">
+    <img class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}">
+    </a>`;
+  })
+  .join("");
+listGallery.insertAdjacentHTML("afterbegin", newArray);
+
+listGallery.addEventListener("click", displayOriginal);
+
+function displayOriginal(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") return;
+  const urlOriginal = event.target.dataset.source;
+  console.dir(event.target);
+  console.log(urlOriginal);
+
+  instance = basicLightbox.create(`
+    <img src="${urlOriginal}" width="800" height="600">
+`);
+
+  instance.show();
+
+  document.addEventListener("keydown", clickEsc);
+}
+
+function clickEsc(event) {
+  if (event.code === "Escape") {
+    instance.close();
+  }
+  document.removeEventListener("keydown", clickEsc);
+}
